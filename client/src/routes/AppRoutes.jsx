b53da883e52ae84695b2c2afa6e8811d0a54d3e7
Imtiaz4530 <smimtiaz58@gmail.com>
2024-09-18 20:43:47 +0600

    5.0.6 add hooks components and pages for profile and termsAndCondition and privacy policy and not found page

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -5,15 +5,30 @@ import { lazy } from "react";
 const HomePage = lazy(() => import("../pages/Home/HomePage"));
 const Login = lazy(() => import("../pages/Auth/Login"));
 const Register = lazy(() => import("../pages/Auth/Register"));
+const Profile = lazy(() => import("../pages/Profile/Profile"));
+const NotFound = lazy(() => import("../pages/NotFound/NotFound"));
+const TermsAndConditions = lazy(() =>
+  import("../pages/TermsAndCondition/TermsAndConditions")
+);
+const PrivacyPolicy = lazy(() =>
+  import("../pages/PrivacyPolicy/PrivacyPolicy")
+);
 
 const AppRoutes = ({ authUser }) => (
   <Routes>
     <Route path="/" element={<HomePage />} />
+    <Route path="/terms" element={<TermsAndConditions />} />
+    <Route path="/privacy" element={<PrivacyPolicy />} />
     <Route path="/login" element={authUser ? <Navigate to="/" /> : <Login />} />
     <Route
       path="/register"
       element={authUser ? <Navigate to="/" /> : <Register />}
     />
+    <Route
+      path="/profile"
+      element={authUser ? <Profile /> : <Navigate to="/login" />}
+    />
+    <Route path="*" element={<NotFound />} />
   </Routes>
 );
 
