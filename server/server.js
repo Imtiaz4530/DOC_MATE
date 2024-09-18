80a49e05bb0b80150a3e9bb76513fa97c7940f89
Imtiaz4530 <smimtiaz58@gmail.com>
2024-09-18 19:31:15 +0600

    3rd client url change on server.js

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: process.env.CLIENT_URL || "https://docauth.onrender.com",
     credentials: true,
   })
 );
